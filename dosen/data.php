658735b41d22339475bb72653219e59d401ce6bc
GUNNERS <naim@palu.linux.or.id>
2011-09-09 03:00:31 +0800

    tambah data.php di modul dosen

---
+++ dosen/data.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ *      ibnu yahya <ibnu.yahya@toroo.org>
+ */
+
+$array_name = array("nip","nama","alamat","kota","kd_jurusan","kd_fakultas","email","web","phone_hp","gelar_s1","gelar_s2","gelar_s3","gelar_gb","gelar_profesi","gol","pangkat","tmt_cpns","tmt_pns","tgl_keluar","karpeg","kelurahan","kecamatan","propinsi","kd_pos","tempat_lahir","tgl_lahir","nama_pasangan","pekerjaan_pasangan","agama","jabatan_organisasi","jabatan_akademik","pendidikan","lulusan_PT_univ","bidang_ilmu","kirim");
+?>
